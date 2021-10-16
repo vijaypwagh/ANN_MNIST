00a0e0a75d9b720faf4f37f49c91264931579675
vijay kr <vijaypwagh15@gmail.com>
2021-10-16 14:25:02 +0530

    get data

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ANN_MNIST
 ANN is applied on MNIST dataset
+## Reference -
+
+* [conda env commands](https://www.google.com/search?q=conda+manage+environments&rlz=1C1CHBF_enIN858IN858&oq=conda+manag&aqs=chrome.0.0i512j69i57j0i512l5j69i60.6480j0j15&sourceid=chrome&ie=UTF-8)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="vijay",
+    description="A small package for ANN Implementation",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vijaypwagh/ANN_MNIST.git",
+    author_email="vijaypwagh15@gmail.com",
+    packages=["src"],
+    python_requires=">=3.7",
+    install_requires=[
+        "tensorflow",
+        "matplotlib",
+        "seaborn",
+        "numpy",
+        "pandas"
+    ]
